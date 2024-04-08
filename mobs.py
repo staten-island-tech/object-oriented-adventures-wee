a85e732a460196b952f2001c68627285c4429522
walterz3 <walterz3@nycstudents.net>
2024-04-08 12:20:21 -0400

    Update mobs.py

---
+++ mobs.py
@@ -0,0 +1,9 @@
+class Mobs:
+    def __init__(mob, name, damage, health, drops):
+        mob.name=name
+        mob.damage=damage
+        mob.health=health
+        mob.drops=drops
+
+Spider=Mobs("spider", "5 dmg", "10 health", "String")
+Zombie=Mobs("zombie", "5 dmg", "15 health", "Rotten Flesh")
