72c6a9167498f3aaa49e979ad96d252bea822bf0
walterz3 <walterz3@nycstudents.net>
2024-05-07 12:28:42 -0400

    Update player.py

--- player.py
+++ player.py
@@ -2,14 +2,8 @@ class Player:
     def __init__(player, health, damage):
         player.health=health
         player.damage=damage
-    health=50
-    damage=10
 
-weapon=input("did you want to buy a wepon")
+Walter=Player(50,100)
 
-if weapon.capitalize() == "Yes":
-    damage=100
-
-
-print(Player)
+print(Walter["health"])
 
