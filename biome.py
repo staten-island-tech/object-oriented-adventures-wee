e593a31ab7fe67991bfc04b9cf40ed102a69b90e
eEricSun <erics314@nycstudents.net>
2024-05-14 12:18:07 -0400

    a

---
+++ biome.py
@@ -0,0 +1,9 @@
+class Biome():
+    def __init__(self,biomename,loot,chances):
+        self.name = biomename
+        self.loot = loot
+        self.chances = chances
+
+
+biomes = ["Badlands","Bamboo","Beach","Birch Forest","Cherry Grove","Cold Ocean","Dark Forest","Deep Ocean","Desert","Flower Forest","Forest","Frozen Peaks","Ice Spikes","Jungle","Mangrove Swamp","Meadow","Mooshroom Island","Ocean","Plains","River","Savanna","Snowy Plains","Snowy Taiga","Sunflower Plains","Swamp","Taiga"]
+biomeweights = [3,2,5,5,2,2,4,2,7,2,9,2,2,3,2,2,1,5,10,4,4,3,3,2,3,7]
