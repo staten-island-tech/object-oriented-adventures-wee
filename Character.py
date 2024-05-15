f41fec641b4fc881bd98c73acae839259ed4e937
walterz3 <walterz3@nycstudents.net>
2024-05-15 12:13:14 -0400

    Create Character.py

---
+++ Character.py
@@ -0,0 +1,18 @@
+class Character:
+    def __init__(player, damage, health, starterweapon):
+        player.damage=damage
+        player.health=health
+        player.starterweapon=starterweapon
+
+Archer=Character(35,15, "bow")
+Beserker=Character(50, 10, "Sword")
+Tank=Character(100, 30, "GreatSword")
+
+class Weapon:
+    def __init__(self, weapon_hitchance, weapon_critchance):
+        self.weapon_hitchance=weapon_hitchance
+        self.weapon_hitchance=weapon_critchance
+    
+bow=Weapon(0.8, 0.5)
+sword=Weapon(0.9, 0.7)
+GreatSoword=Weapon(0.5, 0.5)
