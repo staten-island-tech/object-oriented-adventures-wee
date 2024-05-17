aa472b9498f41a1865063402908945c55afe5eae
eEasonchen <158204132+eEasonchen@users.noreply.github.com>
2024-05-16 21:13:37 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -16,3 +16,32 @@ class Inventory:
             print("Item name:", Data["Name"])
             print("Item quantity:", Data["Quantity"])
             print("Sell value:", Data["Sell value"])
+
+
+# Libraries We Need #
+import json #built in stuff we finna need this later
+
+class InventoryInstance:
+    def __init__(self, data, SlotName) -> None:
+        self.data: dict = data # Dictionary
+        self.inventory: list[dict] = data[SlotName]["Inventory"] # List of dictionaries
+
+    def UpdateData(self):
+        with open('Python\Prac\EasonStuff\Data.json', mode='w') as outfile:
+            json.dump(self.data, outfile, indent=4)
+    
+    def InventoryAdd(self, ItemToAdd, ItemData):
+        self.inventory.append({ItemToAdd: ItemData})
+        self.UpdateData()
+
+    def RemoveItem(self, ItemToRemove: dict):
+        self.inventory.remove(ItemToRemove)
+        self.UpdateData()
+
+
+with open("Python\Prac\EasonStuff\Data.json", mode='r') as infile:
+    Data = json.load(infile)
+
+PracticeInventoryInstance = InventoryInstance(Data, "Slot1")
+
+PracticeInventoryInstance.RemoveItem({"SkibidiToilet": {"Description": "SKIBIDI TOIL!!!"}})
